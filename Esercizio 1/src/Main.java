53224cca3d71be6fba3b92c688e6aac7af780c51
Marco Armenise <armenise.marco@panettipitagora.edu.it>
2023-04-11 17:56:01 +0200

    Modificati file

--- Esercizio 1/src/Main.java
+++ Esercizio 1/src/Main.java
@@ -13,14 +13,3 @@ public class Main {
         consumatore3.start();
     }
 }
-
-/*
-Questo codice crea un'applicazione multithread che utilizza un'istanza della classe "QuotazioniBorsa" come oggetto
-condiviso tra i thread produttore e consumatori.
-
-Il thread produttore genera quotazioni casuali della borsa e le inserisce nell'oggetto "QuotazioniBorsa".
-I thread consumatori recuperano e stampano le quotazioni dalla "QuotazioniBorsa".
-
-L'avvio dei thread viene gestito tramite il metodo "start()", che avvia l'esecuzione del codice all'interno di ogni thread in modo asincrono.
-In questo modo, tutti i thread possono lavorare contemporaneamente e accedere all'oggetto condiviso in modo sincronizzato.
-*/
